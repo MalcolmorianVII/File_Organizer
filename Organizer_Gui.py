1943385046710f537bfedd06c657d8b893c1c0f7
@BelsonKutambe <bdkutambe@gmail.com>
2021-01-12 23:32:48 +0200

    More changes on the GUI

--- Organizer_Gui.py
+++ Organizer_Gui.py
@@ -17,10 +17,17 @@ def check():
 
 
 root = Tk()
-default = StringVar()
-custom = StringVar()
+var = StringVar(root, "1")
 
+mainframe = ttk.Frame(root, padding="3 3 12 12")
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+mainframe.columnconfigure(0, weight=1)
+mainframe.rowconfigure(0, weight=1)
 
+default_button = Radiobutton(mainframe, text="default", variable=var, value="Default")
+default_button.grid(column=5,row=2,sticky=(W,E))
+custom_button = Radiobutton(mainframe, text="custom", variable=var, value="Custom")
+custom_button.grid(column=5,row=4,sticky=(W,E))
 # var = StringVar(root, "1")
 # R1 = Radiobutton(root, text="All", variable=var, value="All")
 # R1.pack(anchor=W)
@@ -36,7 +43,7 @@ custom = StringVar()
 
 
 # code engine & GUI linkage
-Organize = Button(root, text="Organize", command=check)
-Organize.pack(anchor=W)
+# Organize = Button(root, text="Organize", command=check)
+# Organize.pack(anchor=W)
 
 root.mainloop()
