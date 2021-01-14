b1de0d49add64b3d1c8d0cd10bf7f21112b6f866
@BelsonKutambe <bdkutambe@gmail.com>
2021-01-14 12:45:07 +0200

    Another modifier

--- Organizer_Gui.py
+++ Organizer_Gui.py
@@ -17,6 +17,7 @@ def check():
 
 
 root = Tk()
+root.title("File Organize")
 var = StringVar(root, "1")
 
 mainframe = ttk.Frame(root, padding="3 3 12 12")
@@ -25,22 +26,21 @@ mainframe.columnconfigure(0, weight=1)
 mainframe.rowconfigure(0, weight=1)
 
 default_button = Radiobutton(mainframe, text="default", variable=var, value="Default")
-default_button.grid(column=5,row=2,sticky=(W,E))
+default_button.grid(column=5, row=2, sticky=(W, E))
 custom_button = Radiobutton(mainframe, text="custom", variable=var, value="Custom")
-custom_button.grid(column=5,row=4,sticky=(W,E))
+custom_button.grid(column=5, row=4, sticky=(W, E))
 
 R_audio = Radiobutton(mainframe, text="Audios", variable=var, value="Audios")
-R_audio.grid(column = 5,row = 5,sticky = (W,E))
+R_audio.grid(column=5, row=5, sticky=(W, E))
 
 R_video = Radiobutton(mainframe, text="Videos", variable=var, value="Videos")
-R_video.grid(column = 5,row = 6, sticky = (W,E))
+R_video.grid(column=5, row=6, sticky=(W, E))
 
 R_docs = Radiobutton(mainframe, text="Documents", variable=var, value="Docs")
-R_docs.grid(column = 5,row = 7,sticky = E)
-
+R_docs.grid(column=5, row=7, sticky=E)
 
 # code engine & GUI linkage
 Organize = Button(mainframe, text="Organize", command=check)
-Organize.grid(column = 5,row = 9,sticky = (W,E))
+Organize.grid(column=5, row=9, sticky=(W, E))
 
 root.mainloop()
