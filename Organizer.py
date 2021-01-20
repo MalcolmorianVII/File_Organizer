a300a7b4ee69cb8f92b9301a192385327cad6cea
@BelsonKutambe <bdkutambe@gmail.com>
2021-01-20 16:06:10 +0200

    Aggregator function for moving files

--- Organizer.py
+++ Organizer.py
@@ -24,49 +24,12 @@ with open("files.json") as d:
 
 software = [".exe", ".apk", ".py", ".java", ".c", ".js", ".htm", ".html", ".rpm"]
 zipped = [".zip", ".rar"]
-pics = [".png",".gif",".svg","jpeg","tiff"]
+pics = [".png", ".gif", ".svg", "jpeg", "tiff"]
 
-
-# def move(*args):
-#     """Moves the compiled files in cur_dir """
-#     for files in args:
-#         if len(files) > 0:
-#             lambda f: shutil.move(f'{cur_dir}/{file}',destination[files])
-
-
-
-# def vids(videos):
-#     if len(videos):
-#         for file in videos:
-#             shutil.move(f'{cur_dir}/{file}', f'{vid_source}')
-#         print('Videos moved successfully')
-#     else:
-#         print("No videos to move")
-#
-#
-# def auds(audios):
-#     if len(audios) > 0:
-#         for file in audios:
-#             shutil.move(f'{cur_dir}/{file}', f'{aud_source}')
-#         print('Audios moved successfully')
-#     else:
-#         print("No audio files to move")
-#
-#
-# def docs(documents):
-#     if len(documents) > 0:
-#         for file in documents:
-#             shutil.move(f'{cur_dir}/{file}', f'{doc_source}')
-#         print('Docs moved successfully')
-#     else:
-#         print('No Documents to move')
-
-
-# alt:doc_files = [file for file in cur_files if os.path.splitext(file)[1] not in vid_json and aud_json]
 
 # file aggregator : mainly for recursive purpose in case subfolders
 
-def aggregator(**args):
+def aggregator():
     """Total file compiler in  a particular folder to facilitate the move to appropriate destination"""
     cur_dir = os.getcwd()
     cur_files = os.listdir()
@@ -84,27 +47,22 @@ def aggregator(**args):
         # Need to chdir here into subfolder
         for folder in folders:
             try:
-                os.chdir(os.path.join(cur_dir,folder))
+                os.chdir(os.path.join(cur_dir, folder))
                 # cur_files = os.listdir(cur_dir)
                 aggregator()
             except NotADirectoryError:
                 continue
 
-    # print(cur_dir + ":" + str(cur_files))
-    nonlocal destination
-    destination = {f'{vid_files}':f'{vid_source}',f'{aud_files}':f'{aud_source}',f'{doc_files}':f'{doc_source}'
-        ,f'{pic_files}':f'{pics_source}'}
-
+    print(cur_dir + ":" + str(cur_files))
+    # destination = {f'{vid_files}': f'{vid_source}', f'{aud_files}': f'{aud_source}', f'{doc_files}': f'{doc_source}'
+    #     , f'{pic_files}': f'{pics_source}'}
+    #
     # for files in destination:
-    #     if files > 0:
-    #         move(files)
+    #     for file in files:
+    #         try:
+    #             shutil.move(f'{cur_dir}/{file}', destination[files])
+    #         except shutil.Error:
+    #             print("Directory is empty!!!")
 
-aggregator()
-# def all():
-#     vids(vid_files)
-#     auds(aud_files)
-#     docs(doc_files)
-# moving the files
-# vids(vid_files)
-# auds(aud_files)
-# docs(doc_files)
+
+# aggregator()
--- Organizer_Gui.py
+++ Organizer_Gui.py
@@ -4,17 +4,19 @@ from tkinter import ttk
 from functools import partial
 
 
-def check():
-    var_value = var.get()
-    if var_value == "All":
-        all()
-    elif var_value == "Audios":
-        auds(aud_files)
-    elif var_value == "Videos":
-        vids(vid_files)
-    else:
-        docs(doc_files)
+# def check():
+#     var_value = var.get()
+#     if var_value == "All":
+#         all()
+#     elif var_value == "Audios":
+#         auds(aud_files)
+#     elif var_value == "Videos":
+#         vids(vid_files)
+#     else:
+#         docs(doc_files)
 
+# def display():
+#     aggregator()
 
 root = Tk()
 root.title("File Organize")
@@ -25,22 +27,22 @@ mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 mainframe.columnconfigure(0, weight=1)
 mainframe.rowconfigure(0, weight=1)
 
-default_button = Radiobutton(mainframe, text="default", variable=var, value="Default")
-default_button.grid(column=5, row=2, sticky=(W, E))
-custom_button = Radiobutton(mainframe, text="custom", variable=var, value="Custom")
-custom_button.grid(column=5, row=4, sticky=(W, E))
-
-R_audio = Radiobutton(mainframe, text="Audios", variable=var, value="Audios")
-R_audio.grid(column=5, row=5, sticky=(W, E))
-
-R_video = Radiobutton(mainframe, text="Videos", variable=var, value="Videos")
-R_video.grid(column=5, row=6, sticky=(W, E))
-
-R_docs = Radiobutton(mainframe, text="Documents", variable=var, value="Docs")
-R_docs.grid(column=5, row=7, sticky=E)
+# default_button = Radiobutton(mainframe, text="default", variable=var, value="Default")
+# default_button.grid(column=5, row=2, sticky=(W, E))
+# custom_button = Radiobutton(mainframe, text="custom", variable=var, value="Custom")
+# custom_button.grid(column=5, row=4, sticky=(W, E))
+#
+# R_audio = Radiobutton(mainframe, text="Audios", variable=var, value="Audios")
+# R_audio.grid(column=5, row=5, sticky=(W, E))
+#
+# R_video = Radiobutton(mainframe, text="Videos", variable=var, value="Videos")
+# R_video.grid(column=5, row=6, sticky=(W, E))
+#
+# R_docs = Radiobutton(mainframe, text="Documents", variable=var, value="Docs")
+# R_docs.grid(column=5, row=7, sticky=E)
 
 # code engine & GUI linkage
-Organize = Button(mainframe, text="Organize", command=check)
+Organize = Button(mainframe, text="Organize", command=aggregator)
 Organize.grid(column=5, row=9, sticky=(W, E))
 
 root.mainloop()
